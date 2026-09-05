e4537b3c81033d2a4c9443bbacc09dfa5652cdfd
Michael Jarvis <michael@jarvis.com>
2026-09-05 16:12:03 -0500

    update version.go to better report version

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,9 +1,21 @@
 // Package version exposes the application version, set by the linker at release build time.
 package version
 
+import "runtime/debug"
+
 // findDupesVersion is the version reported by the application. Release builds
 // set it with Go linker flags.
 var findDupesVersion = "dev"
+
+func init() {
+	if findDupesVersion != "dev" {
+		return
+	}
+
+	if buildInfo, ok := debug.ReadBuildInfo(); ok && buildInfo.Main.Version != "" && buildInfo.Main.Version != "(devel)" {
+		findDupesVersion = buildInfo.Main.Version
+	}
+}
 
 // Version returns the current version of the application.
 func Version() string {
